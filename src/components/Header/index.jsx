2981a9c9da73a32ccacc2922053a46ae75193962
Kristina Shkoda <kristinashkoda3@gmail.com>
2023-07-12 16:39:58 +0300

    add auth

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,11 +3,19 @@ import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom'
 import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectIsAuth } from '../../redux/slices/auth';
 
 export const Header = () => {
-  const isAuth = false;
+  const dispatch = useDispatch()
+  const isAuth = useSelector(selectIsAuth);
 
-  const onClickLogout = () => { };
+  const onClickLogout = () => {
+    if (window.confirm('Are u sure u want to logout')) {
+      dispatch(logout())
+      window.localStorage.removeItem('token')
+    }
+  };
 
   return (
     <div className={styles.root}>
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -5,24 +5,69 @@ import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 
 import styles from "./Login.module.scss";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAuth, selectIsAuth } from "../../redux/slices/auth";
+import { Navigate } from "react-router-dom";
 
 export const Login = () => {
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid }
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: ''
+    },
+    mode: 'onChange'
+  })
+
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchAuth(values))
+
+    if (!data.payload) {
+      return alert('failed to auth')
+    }
+
+    if ('token' in data.payload) {
+      window.localStorage.setItem('token', data.payload.token)
+    } else {
+      alert('failed to auth')
+    }
+  }
+
+  if (isAuth) {
+    return <Navigate to={"/"} />
+  }
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Неверно указана почта"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          type="email"
+          {...register('email', { required: 'Your e-mail' })}
+          fullWidth
+        />
+        <TextField className={styles.field} label="Пароль"
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          {...register('password', { required: 'Your password' })}
+          fullWidth />
+        <Button type="submit" size="large" variant="contained" fullWidth>
+          Войти
+        </Button>
+      </form>
     </Paper>
   );
 };
